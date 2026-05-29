b4e328c39eeb3269743d6b4bb28a2f3b8fa4db08
tdvipp <tuan.dq1210@gmail.com>
2026-05-30 00:01:51 +0700

    added 01-03

---
+++ 02-source-dest-folders/src/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        String message = new Butler().sayHello();
+        System.out.println(message);
+    }    
+}
---
+++ 03-package-as-jar/src/Butler.java
@@ -0,0 +1,5 @@
+public class Butler {
+    public String sayHello() {
+        return "Good morning Sir";
+    }
+}
